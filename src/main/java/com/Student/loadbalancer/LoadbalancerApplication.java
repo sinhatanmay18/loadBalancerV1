9624b2e7bd275c4cf455ad96161d8122e00c65c7
Ranjan <branjan@informatica.com>
2022-08-02 00:18:22 +0530

    Added comment line

--- src/main/java/com/Student/loadbalancer/LoadbalancerApplication.java
+++ src/main/java/com/Student/loadbalancer/LoadbalancerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoadbalancerApplication {
 
 	public static void main(String[] args) {
+		//comment line
 		SpringApplication.run(LoadbalancerApplication.class, args);
 	}
 
